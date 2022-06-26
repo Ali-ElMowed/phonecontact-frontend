c64122bcba01167694d7ef55a21e3103404ba632
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-26 18:29:06 +0300

    Fetched data to get all contacts

--- src/pages/ViewContacts.js
+++ src/pages/ViewContacts.js
@@ -1,14 +1,43 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const ViewContacts = () => {
+    const navigate = useNavigate();
+    const [contacts, setContacts] = useState([]);
+
+    const getContacts = async () => {
+        const res = await fetch("http://localhost:3000/api/contact/getContacts");
+        const data = res.json();
+        return data;
+    }
+
+    useEffect(() => {
+        const getData = async () => {
+            const contacsFromSurvey = await getContacts();
+            setContacts(contacsFromSurvey);
+            console.log(contacsFromSurvey);
+        };
+        getData();
+    }, []);
 
 
 
-
-
-    
-    return(
-        <>
-        <h3>ALI</h3>
-        </>
+    return (
+        <div className="container">
+            {contacts.map((contact) => {
+                return (
+                    <div
+                        key={contact._id}
+                        className="contact"
+                        onClick={() => {
+                            navigate(`${contact._id}`)
+                        }}>
+                        <h2>{contact.name}</h2>
+                        <button>Click me</button>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
 export default ViewContacts;
