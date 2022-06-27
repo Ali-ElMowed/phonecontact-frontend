bec15751d9265aea45c11f8e2e3e077a5127c323
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-27 04:38:39 +0300

    Added the search and delete functions

--- src/pages/ViewContacts.js
+++ src/pages/ViewContacts.js
@@ -1,42 +1,75 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { getContacts } from "../api/contacts";
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { deleteContact, getContacts } from "../api/contacts"
 
-const ViewContacts = () => {
-    const navigate = useNavigate();
-    const [contacts, setContacts] = useState([]);
+    const ViewContacts = () => {
+        const navigate = useNavigate();
+        const [contacts, setContacts] = useState([]);
+        const [contactId , setContactId] = useState('')
+        const [search,setSearch] = useState('')
 
+        const handleDelete = async (contact_id) => {
+            try {
+                // const contact_id = contactId
+                const res = await deleteContact(contact_id);
+            } catch (error) {
+                console.log(error);
+            }
 
-    useEffect(() => {
-        const getData = async () => {
-            const contacsFromSurvey = await getContacts();
-            setContacts(contacsFromSurvey?.data?.result);
         };
-        getData();
-    }, []);
+
+        useEffect(() => {
+            const getData = async () => {
+                const contacsFromSurvey = await getContacts();
+                setContacts(contacsFromSurvey?.data?.result);
+            };
+            getData();
+        }, []);
 
 
 
 
-    return (
-        <div className="container">
-            <button onClick={() => {
-                navigate('/add')
-            }}>Add Contact</button>
-            {contacts.length > 0 ? contacts?.map((contact) => {
-                return (
-                    <div
-                        key={contact._id}
-                        className="contact"
-                        onClick={() => {
-                            navigate(`${contact._id}`)
-                        }}>
-                        <h2>{contact.name}</h2>
-                        <button >Delete Contact</button>
-                    </div>
-                );
-            }) : <h1>No Contacts Found</h1>}
-        </div>
-    );
-}
-export default ViewContacts;
+        return (
+            <div className="contacts-container">
+                <button onClick={() => {
+                    navigate('/add')
+                }}>Add Contact</button>
+                <input placeholder='Search...' onChange={(e)=>{setSearch(e.target.value)}} value={search}/>
+
+                {contacts.length > 0 ? search.length>0?contacts?.filter(c=>c.email?.includes(search)).map((contact) =>(
+                        <div
+                            key={contact._id}
+                            className="contact" >
+                                
+                            {// onClick={() => {
+                            //     navigate(`${contact._id}`)
+                            // }}>
+                }
+                            <h2>{contact.name}</h2>
+                            <button onClick={(e)=>{
+                                setContactId(e.target.value)
+                                handleDelete(contact._id)
+                            }} value={contact._id}>Delete Contact</button>
+                        </div>
+                    )
+                ) : contacts?.map((contact) =>(
+                        <div
+                            key={contact._id}
+                            className="contact" >
+                                
+                            {// onClick={() => {
+                            //     navigate(`${contact._id}`)
+                            // }}>
+                }
+                            <h2>{contact.name}</h2>
+                            <button onClick={(e)=>{
+                                setContactId(e.target.value)
+                                handleDelete(contact._id)
+                            }} value={contact._id}>Delete Contact</button>
+                        </div>
+                    )
+                ) : <h1>No Contacts Found</h1>}
+            </div>
+        );
+    }
+    export default ViewContacts;
