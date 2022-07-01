d89ae357ba8e74b11ee942870675c45d26b14c20
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-07-01 18:21:17 +0300

    Updated the contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,34 +1,43 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { getContactById } from "../api/contacts";
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
-const ContactPage = ()=> {
-    const {id} = useParams();
-    const [contactContect , setContactContect] = useState([]);
 
-    const getContent = async ()=> {
-        const res = await fetch("http://localhost:3000/api/contact/getContacts",{
-            method:"POST",
-            headers: {
-                "Content-type": "application/json",
-              },
-              body: JSON.stringify({
-                _id:"62b5ed6c2a0024dfaddfa1a3"
-              }),
-        });
-        const data = res.json();
-        return data
-    }
+const ContactPage = () => {
+    const { id } = useParams();
 
-    useEffect(()=>{
-        const getData = async ()=>{
-            const contactContentS = await getContent();
-            setContactContect(contactContentS);
-        };
+    const [contact, setContact] = useState(null);
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const contactById = await getContactById(id)
+                setContact(contactById?.data)
+            } catch (error) {
+                console.log(error);
+            }
+        }
         getData()
-        console.log(getData);
-    },[]);
+    }, [])
 
-
+    return (
+        <div>
+            {contact ? <>
+                <div className="contact-name">{contact?.name}</div>
+                <div className="contact-phone">{contact?.phone_number}</div>
+                <div className="contact-email">{contact?.email}</div>
+                <div className="contact-relation">{contact?.relation_status}</div>
+                <MapContainer style={{ height: 300 }} center={{ lat: contact?.lat, lng: contact?.lng }} zoom={25} scrollWheelZoom={false} className="map">
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={{ lat: contact?.lat, lng: contact?.lng }}>
+                        <Popup>{contact?.name} is here</Popup>
+                    </Marker>
+                </MapContainer></> : <h1>Loading....</h1>}
+        </div>
+    )
 }
 
 export default ContactPage;
