55085a57798db989365a034d0e2f169a4628ec5f
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-07-01 18:21:57 +0300

    Completed the delete feature

--- src/pages/ViewContacts.js
+++ src/pages/ViewContacts.js
@@ -2,74 +2,71 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { deleteContact, getContacts } from "../api/contacts"
 
-    const ViewContacts = () => {
-        const navigate = useNavigate();
-        const [contacts, setContacts] = useState([]);
-        const [contactId , setContactId] = useState('')
-        const [search,setSearch] = useState('')
+const ViewContacts = () => {
+    const navigate = useNavigate();
+    const [contacts, setContacts] = useState([])
+    const [search, setSearch] = useState('')
 
-        const handleDelete = async (contact_id) => {
-            try {
-                // const contact_id = contactId
-                const res = await deleteContact(contact_id);
-            } catch (error) {
-                console.log(error);
-            }
+    const handleDelete = async (id) => {
+        try {
+            const res = await deleteContact(id).then(() => {
+                getData()
+            })
+        } catch (error) {
+            console.log(error);
+        }
 
-        };
-
-        useEffect(() => {
-            const getData = async () => {
-                const contacsFromSurvey = await getContacts();
-                setContacts(contacsFromSurvey?.data?.result);
-            };
-            getData();
-        }, []);
-
+    };
+    const getData = async () => {
+        const contacsFromSurvey = await getContacts();
+        setContacts(contacsFromSurvey?.data?.result);
+    };
+    useEffect(() => {
+        getData();
+    }, []);
 
 
 
-        return (
-            <div className="contacts-container">
-                <button onClick={() => {
-                    navigate('/add')
-                }} >Add Contact</button>
-                <input placeholder='Search...' onChange={(e)=>{setSearch(e.target.value)}} value={search} className="search"/>
 
-                {contacts.length > 0 ? search.length>0?contacts?.filter(c=>c.email?.includes(search)).map((contact) =>(
-                        <div
-                            key={contact._id}
-                            className="contact" >
-                                
-                            {// onClick={() => {
-                            //     navigate(`${contact._id}`)
-                            // }}>
-                }
-                            <h2>{contact.name}</h2>
-                            <button onClick={(e)=>{
-                                setContactId(e.target.value)
-                                handleDelete(contact._id)
-                            }} value={contact._id}>Delete Contact</button>
-                        </div>
-                    )
-                ) : contacts?.map((contact) =>(
-                        <div
-                            key={contact._id}
-                            className="contact" >
-                                
-                            {// onClick={() => {
-                            //     navigate(`${contact._id}`)
-                            // }}>
-                }
-                            <h2>{contact.name}</h2>
-                            <button onClick={(e)=>{
-                                setContactId(e.target.value)
-                                handleDelete(contact._id)
-                            }} value={contact._id}>Delete Contact</button>
-                        </div>
-                    )
-                ) : <h1>No Contacts Found</h1>}
-            </div>
-        );
-    }
-    export default ViewContacts;
+    return (
+        <div className="contacts-container">
+            <button onClick={() => {
+                navigate('/add')
+            }} >Add Contact</button>
+            <input placeholder='Search...' onChange={(e) => { setSearch(e.target.value) }} value={search} className="search" />
+
+            {contacts.length > 0 ? search.length > 0 ? contacts?.filter(c => c.email?.includes(search)).map((contact) => (
+                <div
+                    key={contact._id}
+                    className="contact"
+                >
+
+                    <h2>{contact.name}</h2>
+                    <button
+                        onClick={() => { handleDelete(contact._id) }}
+                        value={contact._id}>
+                        Delete Contact
+                    </button>
+                </div>
+            )
+            ) : contacts?.map((contact) => (
+                <div
+                    key={contact._id}
+                    className="contact"
+
+                >
+
+                    <h2 onClick={() => {
+                        navigate(`/contact/${contact._id}`)
+                    }} >{contact.name}</h2>
+                    <button onClick={() => { handleDelete(contact._id) }
+                        // setContactId(e.target.value)
+
+                    } value={contact._id}>Delete Contact</button>
+                </div>
+            )
+            ) : <h1>No Contacts Found</h1>}
+        </div>
+    );
+}
+export default ViewContacts;
